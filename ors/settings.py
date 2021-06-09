2b17a57c3fc344be865a088d356c1b8b19b79eed
Ankit Kumar <55847022+methelegend-sys@users.noreply.github.com>
2021-06-09 22:30:58 +0530

    Update settings.py

--- ors/settings.py
+++ ors/settings.py
@@ -84,13 +84,10 @@ WSGI_APPLICATION = 'ors.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'djangodb',
-        'USER': 'dbadmin',
-        'PASSWORD': '12345',
-        'HOST': 'localhost',
-        'PORT': '3306',
+    'default': 
+    {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
